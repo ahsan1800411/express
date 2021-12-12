2eb0047cc070c62ec86bc3ef5ab470b118e9561d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-12-12 05:16:28 +0500

    Update

--- routes/user.js
+++ routes/user.js
@@ -13,13 +13,70 @@ router.post('/new', (req, res) => {
 
 // read/find a user
 
-router.get('/get/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.json(user);
-});
-router.get('/get', async (req, res) => {
-  const user = await User.find({});
-  res.json(user);
-});
+// router.get('/get/:id', async (req, res) => {
+//   const user = await User.findById(req.params.id);
+//   res.json(user);
+// });
+// router.get('/get', async (req, res) => {
+//   const user = await User.find({});
+//   res.json(user);
+// });
+
+// // update a user  1st way;
+// router.put('/update/:id', async (req, res) => {
+//   const newData = {
+//     name: req.body.name,
+//     email: req.body.email,
+//   };
+//   await User.findByIdAndUpdate(req.params.id, newData, {
+//     new: true,
+//     runValidators: true,
+//     useFindAndModify: false,
+//   });
+
+// res.json('Update Successfully');
+// });
+
+// update a user by 2nd way;
+
+// router.put('/update/:id', async (req, res) => {
+
+//   await User.findByIdAndUpdate(req.params.id, req.body, {
+//     new: true,
+//     runValidators: true,
+//     useFindAndModify: false,
+//   });
+
+//   res.json('Update Successfully');
+// });
+
+// update a user by 3rd way;
+
+// router.put('/update', async (req, res) => {
+//   await User.updateOne({ name: 'Ahsan Mumtaz' }, req.body, {
+//     new: true,
+//     runValidators: true,
+//     useFindAndModify: false,
+//   });
+//   res.json('Update Successfully');
+// });
+
+// Delete a user;
+
+// router.delete('/delete/:id', async (req, res) => {
+//   await User.findByIdAndRemove(req.params.id);
+
+//   res.json('Delete Successfully');
+// });
+
+// router.delete('/delete/:id', async (req, res) => {
+//   const user = await User.findById(req.params.id);
+
+//   if (!user) {
+//     return res.status(404).json({ message: 'User not found' });
+//   }
+
+//   await user.remove();
+// });
 
 module.exports = router;
